ce72f7102d7b722973afeccb4d7dfaf36ddebcbf
mochoping <hodolskm@gmail.com>
2025-02-27 16:43:28 +0900

    clothes 추가

---
+++ src/main/java/kh/edu/react/provider/mapper/ClothesMapper.java
@@ -0,0 +1,18 @@
+package kh.edu.react.provider.mapper;
+
+import kh.edu.react.provider.dto.Clothes;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+
+@Mapper
+public interface ClothesMapper {
+    List<Clothes> getAllClothes();
+    Clothes getClothesById(int cId);
+    void insertClothes(Clothes clothes);
+    void updateClothes(Clothes clothes);
+    void deleteClothes(int cId);
+
+
+}
